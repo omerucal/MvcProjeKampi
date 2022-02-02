315bf82b037556e49546cf8a6c420cb2d8c6b35e
omerucal <omeraviv533@gmail.com>
2022-02-01 23:52:41 -0800

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        //GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        //public List<Category> GetAllBLL()
+        //{
+        //    return repo.List();
+        //}
+
+        //public void CategoryAddBLL(Category p)
+        //{
+        //    if (p.CategoryName == String.Empty ||
+        //        p.CategoryName.Length <= 3 ||
+        //        p.CategoryDescription == String.Empty ||
+        //        p.CategoryName.Length >= 51)
+        //    {
+        //        //hata mesajı
+        //    }
+        //    else
+        //    {
+        //        repo.Insert(p);
+        //    }
+        //}
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
