0bb725ec288ce256bfad179dc35dbe168f76fe5f
omerucal <omeraviv533@gmail.com>
2022-02-03 07:46:29 -0800

    2022_02_03_19_46_new_commits

---
+++ BusinessLayer/Abstract/IWriterService.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IWriterService
+    {
+        List<Writer> GetListBLL();
+
+        void AddWriterBLL(Writer writer);
+
+        Writer GetWriterByIDBLL(int id);
+
+        void DeleteWriterBLL(Writer writer);
+
+        void UpdateWriterBLL(Writer writer);
+    }
+}
---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterManager : IWriterService
+    {
+        IWriterDal _writerDal;
+
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+
+        public void AddWriterBLL(Writer writer)
+        {
+            _writerDal.Insert(writer);
+        }
+
+        public void DeleteWriterBLL(Writer writer)
+        {
+            _writerDal.Delete(writer);
+        }
+
+        public List<Writer> GetListBLL()
+        {
+            return _writerDal.List();
+        }
+
+        public Writer GetWriterByIDBLL(int id)
+        {
+            return _writerDal.Get(x => x.WriterId == id);
+        }
+
+        public void UpdateWriterBLL(Writer writer)
+        {
+            _writerDal.Update(writer);
+        }
+    }
+}
